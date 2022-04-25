2ce95941ff1b41413bd6828d267c59db34a891c7
Aditya Gupta <97892100+aditya-engg-inbound@users.noreply.github.com>
2022-04-25 14:45:34 +0530

    flutter version upgraded - web issue resolved (#32)
    * flutter version upgraded

--- example/lib/generated_plugin_registrant.dart
+++ example/lib/generated_plugin_registrant.dart
@@ -2,17 +2,19 @@
 // Generated file. Do not edit.
 //
 
-// ignore: unused_import
-import 'dart:ui';
+// ignore_for_file: directives_ordering
+// ignore_for_file: lines_longer_than_80_chars
 
 import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:firebase_messaging_web/firebase_messaging_web.dart';
 import 'package:moengage_flutter_web/moengage_flutter_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
-void registerPlugins(PluginRegistry registry) {
-  FirebaseCoreWeb.registerWith(registry.registrarFor(FirebaseCoreWeb));
-  MoEngageFlutterPlugin.registerWith(registry.registrarFor(MoEngageFlutterPlugin));
-  registry.registerMessageHandler();
+void registerPlugins(Registrar registrar) {
+  FirebaseCoreWeb.registerWith(registrar);
+  FirebaseMessagingWeb.registerWith(registrar);
+  MoEngageFlutterPlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
 }
