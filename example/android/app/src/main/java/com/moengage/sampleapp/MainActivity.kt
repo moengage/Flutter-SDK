e5af839208a29e55684172e0e758f33e2de8b110
arshiya-moengage <arshiya.khanum@moengage.com>
2022-09-12 17:28:32 +0530

    added deeplink in sample app for android

--- example/android/app/src/main/java/com/moengage/sampleapp/MainActivity.kt
+++ example/android/app/src/main/java/com/moengage/sampleapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.moengage.sampleapp
 
+import android.content.Intent
 import android.content.res.Configuration
 import android.os.Bundle
 import android.util.Log
@@ -10,11 +11,12 @@ import io.flutter.embedding.engine.FlutterEngine
 class MainActivity : FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        processIntent(intent)
     }
 
     override fun onConfigurationChanged(newConfig: Configuration) {
         super.onConfigurationChanged(newConfig)
-        Log.d("MainActivity", " : onConfigurationChanged() : " + newConfig.orientation)
+        Log.d("MainActivity", " : onConfigurationChanged() : ${newConfig.orientation}")
         // Checks the orientation of the screen
         getInstance().onConfigurationChanged()
     }
@@ -22,5 +24,15 @@ class MainActivity : FlutterActivity() {
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
         //    GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        processIntent(intent)
+    }
+
+    private fun processIntent(intent: Intent?) {
+        if (intent == null) return
+        Log.d("MainActivity", " : processIntent() : ${intent.data}")
     }
 }
