2a474a9e1b80c96e7850e488a74387cb8d89f644
rakshitha-engg-sdk <rakshitha@moengage.com>
2022-09-20 15:53:31 +0530

    MOEN 18368 Sample app: tappable flutter viewController to handle touch event

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -5,23 +5,54 @@ import MoEngageSDK
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
+    var flutterViewController: FlutterViewController?
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    
-    let yourAppID = "DAO6UGZ73D9RTK8B5W96TPYN" //App ID: You can be obtain it from App Settings in MoEngage Dashboard.
-    let sdkConfig = MOSDKConfig(withAppID: yourAppID)
-    sdkConfig.appGroupID = "group.com.alphadevs.MoEngage.NotificationServices"
-    sdkConfig.enableLogs = true
-    
-    MoEngageInitializer.sharedInstance.initializeDefaultInstance(sdkConfig, launchOptions: launchOptions)
-    
-    MoEngage.enableSDKLogs(true)
       
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+      let yourAppID = "DAO6UGZ73D9RTK8B5W96TPYN" //App ID: You can be obtain it from App Settings in MoEngage Dashboard.
+      let sdkConfig = MOSDKConfig(withAppID: yourAppID)
+      sdkConfig.appGroupID = "group.com.alphadevs.MoEngage.NotificationServices"
+      sdkConfig.enableLogs = true
+      
+      MoEngageInitializer.sharedInstance.initializeDefaultInstance(sdkConfig, launchOptions: launchOptions)
+      
+      MoEngage.enableSDKLogs(true)
+      
+      
+      flutterViewController = FlutterViewController()
+      let nav = UINavigationController.init(rootViewController: flutterViewController!)
+      nav.isNavigationBarHidden = true
+      
+      let window = UIWindow()
+      self.window = window
+      window.rootViewController = nav
+      
+      GeneratedPluginRegistrant.register(with: self)
+      return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+
+        if let vc = UIApplication.shared.keyWindow?.rootViewController as? FlutterViewController {
+            return
+        }
+    }
+
+    override func registrar(forPlugin: String) -> FlutterPluginRegistrar {
+        return (self.flutterViewController?.registrar(forPlugin: forPlugin))!
+    }
+
+    override func hasPlugin(_ pluginKey: String) -> Bool {
+        return (self.flutterViewController?.hasPlugin(pluginKey))!
+    }
+
+    override func valuePublished(byPlugin pluginKey: String) -> NSObject {
+        return (self.flutterViewController?.valuePublished(byPlugin: pluginKey))!
+    }
     
     override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
         print("Opening deeplink", url)
