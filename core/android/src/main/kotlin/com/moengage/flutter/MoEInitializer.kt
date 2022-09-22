c8c418c3309e768c0865f259bb1f1774d75eb61a
arshiya-moengage <arshiya.khanum@moengage.com>
2022-09-22 15:51:24 +0530

    removed secondary instance initializer

--- core/android/src/main/kotlin/com/moengage/flutter/MoEInitializer.kt
+++ core/android/src/main/kotlin/com/moengage/flutter/MoEInitializer.kt
@@ -101,55 +101,5 @@ class MoEInitializer {
                 Logger.print(LogLevel.ERROR, t) { "$tag initialiseDefaultInstance() : " }
             }
         }
-
-
-        /**
-         * Initialise the SDK with configuration provided in [MoEngage.Builder]
-         *
-         * @param builder: Instance of [MoEngage.Builder]
-         */
-        @JvmStatic
-        public fun initialiseInstance(builder: MoEngage.Builder) {
-            try {
-                Logger.print { "$tag initialiseInstance() : Will try to initialize the sdk." }
-                PluginInitializer.initialize(
-                    builder,
-                    IntegrationMeta(
-                        INTEGRATION_TYPE,
-                        MOENGAGE_FLUTTER_LIBRARY_VERSION
-                    )
-                )
-            } catch (t: Throwable) {
-                Logger.print(LogLevel.ERROR, t) { "$tag initialiseInstance() : " }
-            }
-        }
-
-        /**
-         * Initialise the SDK with configuration provided in [MoEngage.Builder] and
-         * SDK state, i.e. whether the SDK should be in enabled or disabled state.
-         *
-         * By default the SDK is enabled. Use this API only if you have a requirement to
-         * enable/disable SDK, else use [MoEngage.initialiseInstance].
-         *
-         * **Note:** State is persisted across session, once the SDK is disabled it will remain
-         * in disabled state until enabled again.
-         *
-         * @param builder: Instance of [MoEngage.Builder]
-         * @param isSdkEnabled true if SDK is enabled, else false.
-         */
-        @JvmStatic
-        @Throws(IllegalStateException::class)
-        public fun initialiseInstance(builder: MoEngage.Builder, isSdkEnabled: Boolean) {
-            try {
-                Logger.print { "$tag initialiseDefaultInstance() : Will try to initialize the sdk." }
-                PluginInitializer.initialize(
-                    builder,
-                    IntegrationMeta(INTEGRATION_TYPE, MOENGAGE_FLUTTER_LIBRARY_VERSION),
-                    if (isSdkEnabled) SdkState.ENABLED else SdkState.DISABLED
-                )
-            } catch (t: Throwable) {
-                Logger.print(LogLevel.ERROR, t) { "$tag initialiseDefaultInstance() : " }
-            }
-        }
     }
 }
