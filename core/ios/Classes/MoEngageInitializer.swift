3805f1d6a67616df43c2738294fef9c36edee148
Rakshitha <>
2022-10-19 10:18:29 +0530

    MOEN-18765-Core: deprecate previous method

--- core/ios/Classes/MoEngageInitializer.swift
+++ core/ios/Classes/MoEngageInitializer.swift
@@ -15,7 +15,13 @@ import MoEngageSDK
     @objc static public let sharedInstance = MoEngageInitializer()
     private override init() {super.init()}
 
-    @objc public func initializeDefaultInstance(_ config: MOSDKConfig, sdkState: MoEngageSDKState = .enabled, launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) {
+    @available(*, deprecated, message: "use 'initializeDefaultInstance(config:sdkState:launchOptions:)'")
+    @objc public func initializeDefaultInstance(_ config: MOSDKConfig, sdkState: Bool = true, launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) {
+        let currentSDKState: MoEngageSDKState = sdkState ? .enabled: .disabled
+        initializeDefaultInstance(config: config, sdkState: currentSDKState, launchOptions: launchOptions)
+    }
+    
+    @objc public func initializeDefaultInstance(config: MOSDKConfig, sdkState: MoEngageSDKState = .enabled, launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) {
         let plugin = MoEngagePlugin()
         plugin.initializeDefaultInstance(sdkConfig: config, sdkState: sdkState, launchOptions: launchOptions)
         plugin.trackPluginInfo(MoEngageFlutterConstants.kPluginName, version: MoEngageFlutterPluginInfo.kVersion)
