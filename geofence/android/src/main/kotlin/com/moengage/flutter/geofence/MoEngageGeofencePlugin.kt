5e2d4a3416ade03ed39cc25e183545a2e6d6efb8
Gowtham KK <gowtham.kk@moengage.com>
2023-02-21 15:53:28 +0530

    Reverted Format Changes in Core Module

--- geofence/android/src/main/kotlin/com/moengage/flutter/geofence/MoEngageGeofencePlugin.kt
+++ geofence/android/src/main/kotlin/com/moengage/flutter/geofence/MoEngageGeofencePlugin.kt
@@ -14,10 +14,6 @@ import io.flutter.plugin.common.MethodChannel.Result
 
 /** MoEngage Geofence Plugin */
 class MoEngageGeofencePlugin : FlutterPlugin, MethodCallHandler {
-    /// The MethodChannel that will the communication between Flutter and native Android
-    ///
-    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-    /// when the Flutter Engine is detached from the Activity
     private lateinit var channel: MethodChannel
 
     private val tag = "MoEngageGeofencePlugin"
