58b2cedbaba7a37fd8590d890eebe7d11665911f
Gowtham KK <gowtham.kk@moengage.com>
2023-05-22 15:42:12 +0530

    Updated Comments

--- core/lib/model/moe_init_config.dart
+++ core/lib/model/moe_init_config.dart
@@ -3,6 +3,7 @@ import 'package:moengage_flutter/model/push/push_config.dart';
 /// Init Config instance to be passed during initialization. If it is not passed
 /// default values will be used.
 class MoEInitConfig {
+  /// Instance of [PushConfig] - Configuration for Handling Push Notification
   PushConfig pushConfig;
 
   MoEInitConfig({required this.pushConfig});
--- core/lib/model/push/push_campaign.dart
+++ core/lib/model/push/push_campaign.dart
@@ -4,6 +4,9 @@ class PushCampaign {
   bool isDefaultAction;
   Map<String, dynamic> clickedAction;
   Map<String, dynamic> payload;
+
+  /// If true, MoEngage SDK will not handle Push redirection for Screen name
+  /// and DeepLinking Push Notifications. Client should handle redirection.
   bool selfHandledPushRedirection = false;
 
   PushCampaign(this.isDefaultAction, this.clickedAction, this.payload,this.selfHandledPushRedirection);
