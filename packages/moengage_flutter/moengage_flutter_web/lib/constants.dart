10601de88359c9241062fd1940b166d9a894a294
Aditya Gupta <97892100+adityagupta-7@users.noreply.github.com>
2025-06-11 12:41:34 +0530

    MOEN-38026 websdk opt out changes (#169)
    * opt out changes websdk MOEN-38026
    
    * updated chanelogs MOEN-38432

--- packages/moengage_flutter/moengage_flutter_web/lib/constants.dart
+++ packages/moengage_flutter/moengage_flutter_web/lib/constants.dart
@@ -13,6 +13,8 @@ const String methodSetUserAttributeSDK = 'add_user_attribute';
 const String methodSetUniqueIdSDK = 'add_unique_user_id';
 const String methodSetAliasSDK = 'update_unique_user_id';
 const String methodLogoutSDK = 'destroy_session';
+const String methodEnableSDK = 'enableSdk';
+const String methodDisableSDK = 'disableSdk';
 
 // Keys Constants
 String keyEventName = 'eventName';
