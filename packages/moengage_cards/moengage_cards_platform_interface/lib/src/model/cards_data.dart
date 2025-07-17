74fb43be1c14fcb72a922dcb7220a7c02d50a857
Gowtham KK <gowtham.kk@moengage.com>
2025-07-17 14:13:50 +0530

    Fix lint

--- packages/moengage_cards/moengage_cards_platform_interface/lib/src/model/cards_data.dart
+++ packages/moengage_cards/moengage_cards_platform_interface/lib/src/model/cards_data.dart
@@ -1,22 +1,23 @@
-import 'package:moengage_flutter/moengage_flutter.dart' show AccessibilityData, keyAccessibility;
+import 'package:moengage_flutter/moengage_flutter.dart'
+    show AccessibilityData, keyAccessibility;
+
 import '../../moengage_cards_platform_interface.dart';
-import './enums/static_image_type.dart';
 
 /// Data for Cards for particular Category
 class CardsData {
   /// [CardsData] Constructor
   CardsData({
-  required this.category,
-  required this.cards,
-  this.staticImagesAccessibilityData,
+    required this.category,
+    required this.cards,
+    this.staticImagesAccessibilityData,
   });
 
   /// Get [CardsData] from Json [Map]
   factory CardsData.fromJson(Map<String, dynamic> json) => CardsData(
-    category: (json[keyCategory] ?? argumentAllCards) as String,
-    cards: parseCardsListFromJson(json[keyCards]),
-    staticImagesAccessibilityData: parseStaticImageAccessibilityData(json[keyAccessibility])
-    );
+      category: (json[keyCategory] ?? argumentAllCards) as String,
+      cards: parseCardsListFromJson(json[keyCards]),
+      staticImagesAccessibilityData:
+          parseStaticImageAccessibilityData(json[keyAccessibility]));
 
   /// Category in which Cards belong to
   String category;
@@ -29,13 +30,14 @@ class CardsData {
 
   /// Convert [CardsData] to Json [Map]
   Map<String, dynamic> toJson() => {
-    keyCategory: category,
-    keyCards: serializeCardsToJson(cards),
-    keyAccessibility: serializeStaticImageAccessibilityData(staticImagesAccessibilityData)
-    };
+        keyCategory: category,
+        keyCards: serializeCardsToJson(cards),
+        keyAccessibility:
+            serializeStaticImageAccessibilityData(staticImagesAccessibilityData)
+      };
 
   @override
   String toString() {
-  return 'CardsData{category: $category, cards: $cards, staticImagesAccessibilityData: $staticImagesAccessibilityData}';
+    return 'CardsData{category: $category, cards: $cards, staticImagesAccessibilityData: $staticImagesAccessibilityData}';
   }
 }
